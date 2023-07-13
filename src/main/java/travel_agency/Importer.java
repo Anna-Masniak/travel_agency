68d33448bbdc1fff0a1f6e68e82f952799b4ca7e
gosia <wheresmyredpillmorpheus@gmail.com>
2023-07-13 19:44:26 +0200

    @SpringBootApplication

--- src/main/java/travel_agency/Importer.java
+++ src/main/java/travel_agency/Importer.java
@@ -1,12 +1,14 @@
 package travel_agency;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static java.lang.Class.forName;
-
+@SpringBootApplication
 public class Importer {
     public static void main(String[] args) {
 
